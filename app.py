fe6a74f291095658a13049e568d2dadcad98a2ce
jz559@cornell.edu <jz559@cornell.edu>
2024-02-08 17:18:50 -0500

    Add app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import *
 from whitenoise import WhiteNoise
 
 app = Flask (__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root = "static/", prefix = "static/", index_file = "index.htm", autorefresh = True)
+# app.wsgi_app = WhiteNoise(app.wsgi_app, root = "static/", prefix = "static/", index_file = "index.htm", autorefresh = True)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root = "4310-hw1/", prefix = "4310-hw1/", index_file = "index.html", autorefresh = True)
 
 
 @app.route('/', methods = ["GET"])
